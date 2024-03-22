1451a43e4c06ec9b4e0cfdaf890795da0cd0b65f
naufal <askpradana@gmail.com>
2024-03-23 05:49:25 +0700

    feat: add basic function for User and Profile table, note in the future pls edit ID to cuid instead of autoincrement

--- index.js
+++ index.js
@@ -2,9 +2,75 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+
 const prisma = new PrismaClient();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
+app.post("/login", async function (req, res) {
+	const email = req.body.email;
+	const hashedPassword = await bcrypt.hash(req.body.password, 10);
+	const user = await prisma.user.findUnique({
+		where: {
+			userEmail: email,
+		},
+	});
+	const userPassword = user.password;
+	const result = bcrypt.compareSync(req.body.password, userPassword);
+	console.log(result);
+	if (user) {
+		res.send(user);
+	} else {
+		res.status(401).send(hashedPassword);
+	}
+});
+
+app.post("/register", async (req, res) => {
+	const { name, email, password } = req.body;
+	const hashedPassword = await bcrypt.hash(password, 10);
+	const user = await prisma.user.create({
+		data: {
+			name,
+			userEmail: email,
+			password: hashedPassword,
+		},
+	});
+	res.send(user);
+});
+
+app.post("/profile", async (req, res) => {
+	const id = req.body.id;
+	const user = await prisma.profile.findFirst({
+		where: {
+			profileID: id,
+		},
+	});
+	if (user) {
+		res.send(user);
+	} else {
+		// in the future, change user id to cuid instead of autoincrement
+		res.send({
+			message: "Profile not found, redirect to fill the profile form",
+		});
+	}
+});
+
+app.post("/profile/add", async (req, res) => {
+	const { id, address, gender, phone, email } = req.body;
+	const user = await prisma.profile.create({
+		data: {
+			profileID: id,
+			address,
+			gender,
+			phone,
+			profileEmail: email,
+		},
+	});
+	if (user) {
+		res.send(user);
+	}
+});
 
 app.get("/", async (req, res) => {
 	const users = await prisma.user.findMany();
