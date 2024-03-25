8814d701a98d83d85db58271efa73b57997b20bd
naufal <askpradana@gmail.com>
2024-03-25 10:11:21 +0700

    delete unused API

--- index.js
+++ index.js
@@ -11,22 +11,6 @@ app.use(morgan("dev"));
 app.use("/auth", authRoute);
 app.use("/settings", settingsRoute);
 
-app.get("/", async (req, res) => {
-	const users = await prisma.user.findMany();
-	res.send(users);
-});
-
-app.post("/add", async (req, res) => {
-	const { name, email } = req.body;
-	const user = await prisma.user.create({
-		data: {
-			name,
-			userEmail: email,
-		},
-	});
-	res.send(user);
-});
-
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
