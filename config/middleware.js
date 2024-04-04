9ee639da852f57333bca0e57c4782aec7b44f2a9
naufal <askpradana@gmail.com>
2024-04-05 05:56:13 +0700

    feat: move the middleware to middleware class for better readable code, add /protecteed for protected use that requires token to proceed

---
+++ config/middleware.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
+const { PrismaClient } = require("@prisma/client");
+
+// get config vars
+dotenv.config();
+
+const prisma = new PrismaClient();
+const tokenChecker = async function (req, res, next) {
+	const { email } = req.body;
+	const token = req.headers.authorization;
+
+	if (!token || !email)
+		return res.status(401).send({ message: "param invalid" });
+
+	const tokenIsValid = await prisma.user.findUnique({
+		where: {
+			userEmail: email,
+		},
+		select: {
+			token: true,
+		},
+	});
+
+	if (!tokenIsValid) return res.status(401).send({ message: "token invalid" });
+
+	const tokenIsCorrect = bcrypt.compareSync(token, tokenIsValid.token);
+
+	if (tokenIsCorrect == false)
+		return res.status(401).send({ message: "incorrect token" });
+
+	try {
+		jwt.verify(token, process.env.TOKEN_SECRET);
+	} catch {
+		return res.status(401).send({ message: "token expired" });
+	}
+
+	next();
+};
+
+module.exports = tokenChecker;
--- index.js
+++ index.js
@@ -9,16 +9,7 @@ var settingsRoute = require("./route/settings");
 var categoryRoute = require("./route/items/category");
 var itemRoute = require("./route/items/item");
 var swaggerConfig = require("./config/swagger");
-
-const bcrypt = require("bcrypt");
-const { PrismaClient } = require("@prisma/client");
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-
-// get config vars
-dotenv.config();
-
-const prisma = new PrismaClient();
+var tokenChecker = require("./config/middleware");
 
 // config
 app.use(bodyParser.json());
@@ -29,43 +20,11 @@ app.use(
 	swaggerUi.setup(swaggerConfig, { explorer: true })
 );
 
-const tokenChecker = async function (req, res, next) {
-	const { email } = req.body;
-	const token = req.headers.authorization;
-
-	if (!token || !email)
-		return res.status(401).send({ message: "param invalid" });
-
-	const tokenIsValid = await prisma.user.findUnique({
-		where: {
-			userEmail: email,
-		},
-		select: {
-			token: true,
-		},
-	});
-
-	if (!tokenIsValid) return res.status(401).send({ message: "token invalid" });
-
-	const tokenIsCorrect = bcrypt.compareSync(token, tokenIsValid.token);
-
-	if (tokenIsCorrect == false)
-		return res.status(401).send({ message: "incorrect token" });
-
-	try {
-		jwt.verify(token, process.env.TOKEN_SECRET);
-	} catch {
-		return res.status(401).send({ message: "token expired" });
-	}
-
-	next();
-};
-
-app.use("/settings", tokenChecker);
+app.use("/protected", tokenChecker);
 
 // Route
 app.use("/auth", authRoute);
-app.use("/settings", settingsRoute);
+app.use("/protected/settings", settingsRoute);
 app.use("/categories", categoryRoute);
 app.use("/items", itemRoute);
 
