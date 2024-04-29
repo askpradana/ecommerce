3526263688d00fa3c6c94ecf7e4eb00dbcb3c708
naufal <askpradana@gmail.com>
2024-04-29 21:29:48 +0700

    feat: add cors support

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ app.use(
 	swaggerUi.serve,
 	swaggerUi.setup(swaggerConfig, { explorer: true })
 );
+// * CORS
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 
 app.use("/protected", tokenChecker);
 
